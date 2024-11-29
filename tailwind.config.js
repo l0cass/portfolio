070d84c6518131cccbf4318c38851cc8b0535e1e
l0cass <piffer.lucas0@gmail.com>
2024-11-29 17:24:05 -0300

    completed portfolio website

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./*.html"],
+  theme: {
+    container: {
+      padding: {
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+      },
+    },
+  },
+  plugins: [],
+}
